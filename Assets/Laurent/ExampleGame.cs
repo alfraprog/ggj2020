2a2ddc06a9e949fd311ec98a483e810795414d12
monkydream <ldoobidoo@gmail.com>
2020-02-02 14:20:25 +0100

    hack for fmod paramter fix cf. Laurent/ExampleGame.cs

--- Assets/Laurent/ExampleGame.cs
+++ Assets/Laurent/ExampleGame.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class ExampleGame : MonoBehaviour
 {
+    public StudioParameterTrigger fmodParam;
+
     // Start is called before the first frame update
     void Start()
     {
-        AudioPlayer.PlaySFX(AudioPlayer.instance.fmodAudio.death);
+        //AudioPlayer.PlaySFX(AudioPlayer.instance.fmodAudio.death);
+        // fmod paramerter fix hack!
+       // GetComponent<StudioParameterTrigger>().enabled = true;
+
     }
 
     // Update is called once per frame
